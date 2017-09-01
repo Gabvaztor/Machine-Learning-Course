dbd44ee61f2610defba98dc6c0a02ea68c47d9f3
gabvaztor <gabvaztor@gmail.com>
2017-09-01 22:46:41 +0200

    Librerías del curso

---
+++ Curse_OpenWebinars/Librerías/Prueba_Librerías.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+print("TensorFlow: " + str(tf.__version__))
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import tflearn
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import cv2 as cv2
+print("Todo OK si no aparece ningún error. Es posible que salga un aviso sobre hdf5 y curses pero en este curso no es necesario.")
